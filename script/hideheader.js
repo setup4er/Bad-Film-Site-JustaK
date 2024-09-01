9ed567ef4a07e90f81dce9ce67ed7c755183c766
ivan-thecat <ivan.andreevich000@gmail.com>
2024-09-01 14:07:22 +0300

    Fixed nav menu styles: *mainstyle.css. Fixed button nav menu for mobile phones.

--- script/hideheader.js
+++ script/hideheader.js
@@ -1,15 +1,14 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
 
-//Кнопка adaptive меню. Опт. для телефонов
-if (navToggle && navMenu) 
-{
-  navToggle.addEventListener('click', () => {
-    console.log('Кнопка меню нажата');
-    navMenu.classList.toggle('active');
-    navToggle.classList.toggle('active');
-  });
-} else 
-{
-  console.error('Не удалось найти элементы меню');
-}
+  if (navToggle && navMenu) {
+      navToggle.addEventListener('click', () => {
+          console.log('Кнопка меню нажата');
+          navMenu.classList.toggle('active');
+          navToggle.classList.toggle('active');
+      });
+  } else {
+      console.error('Не удалось найти элементы меню');
+  }
+});
